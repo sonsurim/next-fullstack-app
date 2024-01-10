a02c39915a24d21d400358eb5485a8d1bae336de
sonsurim <surim014@naver.com>
2024-01-11 01:25:57 +0900

    add drafts functionality

---
+++ pages/drafts.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import Layout from '../components/Layout';
+import Post, { PostProps } from '../components/Post';
+import prisma from '../lib/prisma';
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const drafts = await prisma.post.findMany({
+    where: {
+      author: { email: process.env.MOCK_SESSION_EMAIL },
+      published: false,
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return {
+    props: { drafts },
+  };
+};
+
+type Props = {
+  drafts: PostProps[];
+};
+
+const Drafts: React.FC<Props> = (props) => {
+  if (props.drafts.length === 0) {
+    return (
+      <Layout>
+        <h1>My Drafts</h1>
+        <div>You need to be authenticated to view this page.</div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <div className="page">
+        <h1>My Drafts</h1>
+        <main>
+          {props.drafts.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+      <style jsx>{`
+        .post {
+          background: var(--geist-background);
+          transition: box-shadow 0.1s ease-in;
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Drafts;
